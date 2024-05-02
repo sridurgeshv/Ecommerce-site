fb46cec9ba60e6351500e628149e249c6d62c7db
Sri Durgesh <60750509+sridurgeshv@users.noreply.github.com>
2024-05-02 23:36:22 +0530

    Update Cart.js

--- src/components/views/Cart.js
+++ src/components/views/Cart.js
@@ -10,7 +10,7 @@ const Cart = ({ cartItems }) => {
   };
 
   const goToCheckout = () => {
-    navigate('/checkout');
+    navigate('/checkout', { state: { cartItems } }); // Pass cartItems as state to checkout route
   };
 
   return (
