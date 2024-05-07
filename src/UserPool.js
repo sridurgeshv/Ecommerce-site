da2419bbed464f953d24291e3eee5c033174848c
Sri Durgesh <60750509+sridurgeshv@users.noreply.github.com>
2024-05-07 18:32:36 +0530

    Delete src directory

--- src/UserPool.js
+++
@@ -1,6 +0,0 @@
-import { CognitoUserPool } from "amazon-cognito-identity-js";
-const poolData = {
-  UserPoolId: "us-east-1_X4cZfd6Ch",
-  ClientId: "sumklqjubpe586cvu0eranr6u",
-};
-export default new CognitoUserPool(poolData);
