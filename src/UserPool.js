216286c0d7d015da9acc8e84c74a4c1bbb7a6491
Sri Durgesh <60750509+sridurgeshv@users.noreply.github.com>
2024-05-25 23:47:24 +0530

    Update UserPool.js

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,7 +1,7 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 const poolData = {
-  UserPoolId: "us-east-1_X4cZfd6Ch",
-  ClientId: "sumklqjubpe586cvu0eranr6u",
+  UserPoolId: "Your-pool-ID",
+  ClientId: "CLIENT-ID",
 };
 const userPool = new CognitoUserPool(poolData);
 
